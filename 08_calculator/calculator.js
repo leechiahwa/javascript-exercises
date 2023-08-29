60389d77cfa998c6e3774eba3f0816c2bfb5abca
leechiahwa <leechiahwa99@hotmail.com>
2023-08-29 23:26:54 +0800

    Solve 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+  result = a + b;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	result = a - b;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((a, b) => a * b);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  result = a ** b;
+	return result;
 };
 
-const factorial = function() {
-	
+const factorial = function factorialize(num) {
+  if (num < 0)
+  {
+    return - 1;
+  }
+  else if (num == 0)
+  {
+    return 1;
+  }
+  else
+  {
+    return (num * factorialize(num - 1));
+  }
 };
 
 // Do not edit below this line
